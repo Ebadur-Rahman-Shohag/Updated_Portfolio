03bd0191f30f62129e1b355809781fbc6322ba4c
Ebadur Rahman <ebadurshohag@gmail.com>
2024-04-21 00:20:50 +0600

    add contact form functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -59,29 +59,52 @@
 
 export default Contact;
 */
-import { useState } from 'react';
-
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
 function Contact() {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   const [formSubmitted, setFormSubmitted] = useState(false);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
     // You can implement form submission logic here
-    console.log('Form submitted:', { fullName, email, message });
-    // Clear form fields after submission
-    setFullName('');
-    setEmail('');
-    setMessage('');
-    setFormSubmitted(true);
+    console.log("Form submitted:", { fullName, email, message });
+
+    emailjs
+      .sendForm("service_e32fl5f", "template_4rax8rn", form.current, {
+        publicKey: "IZ6lbxloIseQ5IYOn",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          // Clear form fields after submission
+          setFullName("");
+          setEmail("");
+          setMessage("");
+          setFormSubmitted(true);
+          toast.success("Message Sent");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
   };
 
   return (
     <div id="contact" className="max-w-md mx-auto my-8">
-      <h1 className="border-b pb-5 text-3xl font-semibold text-center mb-4">Get in Touch</h1>
-      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mt-8 mb-4">
+      <h1 className="border-b pb-5 text-3xl font-semibold text-center mb-4">
+        Get in Touch
+      </h1>
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mt-8 mb-4"
+      >
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
@@ -93,6 +116,7 @@ function Contact() {
             className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
             id="fullName"
             type="text"
+            name="user_name"
             placeholder="Enter your full name"
             value={fullName}
             onChange={(e) => setFullName(e.target.value)}
@@ -110,6 +134,7 @@ function Contact() {
             className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
             id="email"
             type="email"
+            name="user_email"
             placeholder="Enter your email address"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -126,6 +151,7 @@ function Contact() {
           <textarea
             className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
             id="message"
+            name="message"
             placeholder="Type your message here"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
@@ -133,11 +159,14 @@ function Contact() {
           />
         </div>
         <button
-          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${formSubmitted && 'opacity-50 cursor-not-allowed'}`}
+          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+            formSubmitted && "opacity-50 cursor-not-allowed"
+          }`}
           type="submit"
+          value="Send"
           disabled={formSubmitted}
         >
-          {formSubmitted ? 'Message Sent' : 'Send Message'}
+          {formSubmitted ? "Message Sent" : "Send Message"}
         </button>
       </form>
     </div>
@@ -145,5 +174,3 @@ function Contact() {
 }
 
 export default Contact;
-
-
