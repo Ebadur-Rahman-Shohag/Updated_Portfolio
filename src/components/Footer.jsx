e0da4e3a29e05d995b84a0ea5636d37ea372489a
Ebadur Rahman <ebadurshohag@gmail.com>
2024-06-12 00:51:31 +0600

    modify footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-[#303837]">
       <div className="align-element py-4 flex justify-center  sm:flex-row sm:gap-x-8 sm:items-center sm:py-8">
-        <div className="text-white mx-auto flex text-center gap-x-4">
+        <div className="text-white mx-auto flex flex-col space-y-2 text-center gap-x-4 md:flex-row md:space-y-0">
           {links.map((link) => {
             const { id, href, text } = link;
             return (
